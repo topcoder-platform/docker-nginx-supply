434d52ded1bc3503e0dc66403732d7c31dfdab6d
Selva Umapathy <tamizhselvanu@gmail.com>
2015-11-20 18:54:07 -0800

    Path error

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ cp -r /data/docker-nginx-supply/src/nginx.conf /data/nginx/
 cp -r /data/docker-nginx-supply/src/limits.conf /data/nginx/
 cp -r /data/docker-nginx-supply/src/sites-enabled /data/nginx/
 cp -r /data/docker-nginx-supply/src/includes /data/nginx/
+
 ln -s /data/nginx/nginx.conf /etc/nginx/nginx.conf
 ln -s /data/nginx/limits.conf /etc/nginx/limits.conf
 ln -s /data/nginx/sites-enabled /etc/nginx/sites-enabled
