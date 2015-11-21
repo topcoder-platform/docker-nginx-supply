f68770615666cb97ff360029a8ba69c4ce99ca68
Selva Umapathy <tamizhselvanu@gmail.com>
2015-11-20 17:39:04 -0800

    Formatting

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
-if [[ $ENV != "dev" && $ENV != "qa" && $ENV != "prod" && $ENV != "local" ]]
-then
+if [ $ENV != "dev" && $ENV != "qa" && $ENV != "prod" && $ENV != "local" ] ; then
 	echo "Usage: ./build.sh ENV"
 	echo "ENV must be one of - dev, qa, prod or local"
 	exit
@@ -16,18 +15,15 @@ cp src/sites-enabled/*conf dist/sites-enabled/
 cp src/includes/*conf dist/includes/
 cp src/*conf dist/
 
-if [ $ENV == "dev" ]
-then
+if [ $ENV == "dev" ] ; then
 	cp -R src/dev dist/
 fi
 
-if [ $ENV == "local" ]
-then
+if [ $ENV == "local" ] ; then
 	cp -R src/local /data/nginx
 fi
 
-if [ $ENV != "prod" ]
-then
+if [ $ENV != "prod" ] ; then
 	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder-$ENV\.com/g" dist/sites-enabled/*conf
 else
 	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder\.com/g" dist/sites-enabled/*conf
