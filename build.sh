1863a476b778b1c6db0d6d59244e711278321e1c
Selva Umapathy <tamizhselvanu@gmail.com>
2015-11-20 17:48:58 -0800

    Fixing bash script errors

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-#!/bin/bash -x
-if [ "$ENV" != "dev" ] && [ "$ENV" != "qa" ]  && [ "$ENV"!= "prod" ] && [ "$ENV" != "local" ] ; then
-	echo "Usage: ./build.sh and environment should be set on startup"
+#!/bin/bash
+
+if [[  -z "$ENV" ]] ; then
+	echo "Environment should be set on startup with one of the below values"
 	echo "ENV must be one of - dev, qa, prod or local"
 	exit
 fi
@@ -15,15 +16,15 @@ cp src/sites-enabled/*conf dist/sites-enabled/
 cp src/includes/*conf dist/includes/
 cp src/*conf dist/
 
-if [ "$ENV" == "dev" ] ; then
+if [[ "$ENV" == dev ]]; then
 	cp -R src/dev dist/
 fi
 
-if [ "$ENV" == "local" ] ; then
+if [[ "$ENV" == local ]]; then
 	cp -R src/local /data/nginx
 fi
 
-if [ "$ENV" != "prod" ] ; then
+if [[ "$ENV" != prod ]]; then
 	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder-$ENV\.com/g" dist/sites-enabled/*conf
 else
 	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder\.com/g" dist/sites-enabled/*conf
