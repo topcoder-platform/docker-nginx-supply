2c5a83c13d62023cc5db1918521410487b2adde5
Selva Umapathy <tamizhselvanu@gmail.com>
2015-11-20 18:52:30 -0800

    Fixing the file path

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-EVM=$ENV
-if [[ -z "$EVM" ]] ; then
+
+if [[ -z "$ENV" ]] ; then
 	echo "Environment should be set on startup with one of the below values"
 	echo "ENV must be one of - dev, qa, prod or local"
 	exit
@@ -16,18 +16,18 @@ cp src/sites-enabled/*conf dist/sites-enabled/
 cp src/includes/*conf dist/includes/
 cp src/*conf dist/
 
-if [[ "$EVM" == dev ]]; then
+if [[ "$ENV" == dev ]]; then
 	cp -R src/dev dist/
 fi
 
-if [[ "$EVM" == local ]]; then
+if [[ "$ENV" == local ]]; then
 	cp -R src/local /data/nginx
 fi
 
-if [[ "$EVM" != prod ]]; then
-	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder-$EVM\.com/g" dist/sites-enabled/*conf
+if [[ "$ENV" != prod ]]; then
+	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder-$ENV\.com/g" dist/sites-enabled/*conf
 else
 	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder\.com/g" dist/sites-enabled/*conf
 fi
 
-perl -pi -e "s/\{\{EVM\}\}/$EVM/g" dist/sites-enabled/*conf
+perl -pi -e "s/\{\{ENV\}\}/$ENV/g" dist/sites-enabled/*conf
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 git pull && git checkout $ENV && git pull
 
-cp -r /data/docker-nginx-supply/nginx.conf /data/nginx/nginx.conf
-cp -r /data/docker-nginx-supply/nginx.conf /data/nginx/limits.conf
-cp -r /data/docker-nginx-supply/nginx.conf /data/nginx/sites-enabled
-cp -r /data/docker-nginx-supply/nginx.conf /data/nginx/includes
+cp -r /data/docker-nginx-supply/src/nginx.conf /data/nginx/
+cp -r /data/docker-nginx-supply/src/limits.conf /data/nginx/
+cp -r /data/docker-nginx-supply/src/sites-enabled /data/nginx/
+cp -r /data/docker-nginx-supply/src/includes /data/nginx/
 ln -s /data/nginx/nginx.conf /etc/nginx/nginx.conf
 ln -s /data/nginx/limits.conf /etc/nginx/limits.conf
 ln -s /data/nginx/sites-enabled /etc/nginx/sites-enabled
