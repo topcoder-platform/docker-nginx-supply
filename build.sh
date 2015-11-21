6ac41851781773960e757a2ff564de782518e47c
Selva Umapathy <tamizhselvanu@gmail.com>
2015-11-20 17:51:11 -0800

    Fixing bash script errors

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+EVM=$ENV
 
-if [[  -z "$ENV" ]] ; then
+if [[ -z "$EVM" ]] ; then
 	echo "Environment should be set on startup with one of the below values"
 	echo "ENV must be one of - dev, qa, prod or local"
 	exit
@@ -16,18 +17,18 @@ cp src/sites-enabled/*conf dist/sites-enabled/
 cp src/includes/*conf dist/includes/
 cp src/*conf dist/
 
-if [[ "$ENV" == dev ]]; then
+if [[ "$EVM" == dev ]]; then
 	cp -R src/dev dist/
 fi
 
-if [[ "$ENV" == local ]]; then
+if [[ "$EVM" == local ]]; then
 	cp -R src/local /data/nginx
 fi
 
-if [[ "$ENV" != prod ]]; then
-	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder-$ENV\.com/g" dist/sites-enabled/*conf
+if [[ "$EVM" != prod ]]; then
+	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder-$EVM\.com/g" dist/sites-enabled/*conf
 else
 	perl -pi -e "s/\{\{ENV_TLD\}\}/topcoder\.com/g" dist/sites-enabled/*conf
 fi
 
-perl -pi -e "s/\{\{ENV\}\}/$ENV/g" dist/sites-enabled/*conf
+perl -pi -e "s/\{\{EVM\}\}/$EVM/g" dist/sites-enabled/*conf
