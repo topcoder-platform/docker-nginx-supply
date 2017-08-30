933c4ae83afc3083e84e11140bc067d25032aec1
Matthew Twomey <mtwomey@beakstar.com>
2017-08-30 00:55:09 -0500

    Updates to allow logging to Logentries

--- init_logentries.sh
+++ init_logentries.sh
@@ -7,7 +7,7 @@ touch /var/log/nginx/logs/error.log
 
 mkdir /etc/le
 cp logentries.config /etc/le/config
-sed -i '' "s/{{user-key}}/`cat /tmp/logentries.key`/g" /etc/le/config
+sed -i "s/{{user-key}}/`cat /tmp/logentries.key`/g" /etc/le/config
 
 le follow /var/log/nginx/logs/access.log
 le follow /var/log/nginx/logs/error.log
