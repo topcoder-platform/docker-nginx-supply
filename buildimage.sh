18a51b0faea6c875faac6cfd9e8e354196292d00
Gunasekar-K <Gunasekar.Kesavalu@wipro.com>
2017-11-22 15:22:44 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -67,7 +67,8 @@ if [[ "$PROVIDER" == local ]]; then
 	docker build --no-cache -t nginx-supply:latest .
 else
 	configure_aws_cli
-	eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+	aws s3 cp "s3://appirio-platform-$ENV/services/common/dockercfg" ~/.dockercfg
+	#eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 	# Builds Docker image of the app.
 	TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
 	docker build -f ECSDockerfile -t $TAG .
