717e26f601f83175f4864f679fb945765cadc3cb
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2019-04-30 13:05:00 +0530

    Update buildimage.sh
    [skip ci]

--- buildimage.sh
+++ buildimage.sh
@@ -8,25 +8,25 @@ if [[ -z "$ENV" ]] ; then
 fi
 
 echo "$ENV before case conversion"
-AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
-AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
-AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
+# AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+# AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
+# AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+# AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
 #APP_NAME
 
 #Converting environment varibale as lower case for build purpose
 ENV=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
 echo "$ENV after case conversion"
 
-configure_aws_cli() {
-	aws --version
-	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-	aws configure set default.region $AWS_REGION
-	aws configure set default.output json
-	echo "Configured AWS CLI."
-}
+# configure_aws_cli() {
+# 	aws --version
+# 	aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+# 	aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+# 	aws configure set default.region $AWS_REGION
+# 	aws configure set default.output json
+# 	echo "Configured AWS CLI."
+# }
 
 rm -rf dist
 mkdir -p dist/sites-enabled
@@ -67,10 +67,11 @@ perl -pi -e "s/\{\{ENV\}\}/$ENV/g" dist/includes/*conf
 if [[ "$PROVIDER" == local ]]; then
 	docker build --no-cache -t nginx-supply:latest .
 else
-	configure_aws_cli
+	# configure_aws_cli
 	aws s3 cp "s3://appirio-platform-$ENV/services/common/dockercfg" ~/.dockercfg
 	#eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
 	# Builds Docker image of the app.
-	TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
+	# TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_SHA1
+	TAG=nginx-supply:latest
 	docker build -f ECSDockerfile -t $TAG .
 fi
